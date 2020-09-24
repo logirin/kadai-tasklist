63e94af76ef27605e9aa974eb4ab21a63c99af19
logirin <70879251+logirin@users.noreply.github.com>
2020-09-24 10:18:49 +0000

    post

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,16 @@
 class TasksController < ApplicationController
-  before_action :set_task,only:[:show,:edit,:update,:destroy]
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy]
+  before_action :set_task,only:[:show,:edit,:update]
   def index
-      @tasks=Task.all
+    if logged_in?
+      @task = current_user.tasks.build
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+    end
   end
 
   def show
+    set_task
   end
 
   def new
@@ -12,18 +18,20 @@ class TasksController < ApplicationController
   end
 
   def create
-      @task=Task.new(task_params)
+      @task = current_user.tasks.build(task_params)
       
       if @task.save
           flash[:success]='Taskが正常に作成されました'
-          redirect_to @task
+          redirect_to root_url
       else
-          flash.now[:danger]='Taskが作成できませんでした'
-          render :new
+        @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+        flash.now[:danger]='Taskが作成できませんでした'
+        render :new
       end
   end
 
   def edit
+    set_task
   end
 
   def update
@@ -40,9 +48,8 @@ class TasksController < ApplicationController
   def destroy
     
     @task.destroy
-
     flash[:success] = 'Taskは正常に削除されました'
-    redirect_to tasks_url
+    redirect_to root_url
   end
 
 
@@ -53,5 +60,12 @@ class TasksController < ApplicationController
   
   def task_params
       params.require(:task).permit(:content,:status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 end
